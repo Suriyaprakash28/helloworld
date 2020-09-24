ba0549decc4d36a596ee61198b03d643eab815eb
unknown <suriyaprakash.m.2017.bme@rajalakshmi.edu.in>
2020-09-24 12:44:16 +0530

    Updates

---
+++ precache-manifest.bf0e9b1532be5e6a998cf9d8874b9204.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deeb30d813ae0e9410c3cf23cecd0dc1",
+    "url": "/helloworld/index.html"
+  },
+  {
+    "revision": "8aad645cd0b157b68a95",
+    "url": "/helloworld/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0e38a54628643abbfd79",
+    "url": "/helloworld/static/js/2.aca5da7f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/helloworld/static/js/2.aca5da7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8aad645cd0b157b68a95",
+    "url": "/helloworld/static/js/main.f8769f7e.chunk.js"
+  },
+  {
+    "revision": "01a0ada911a03a48c5ff",
+    "url": "/helloworld/static/js/runtime-main.35c4ead0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/helloworld/static/media/logo.5d5d9eef.svg"
+  }
+]);
